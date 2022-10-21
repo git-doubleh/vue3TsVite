085787a2aa719b65296834d7b53a532b22931bf0
huanghao <huanghao@deltaphone.com.cn>
2022-10-21 14:40:45 +0800

    add vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @Date: 2022-10-21 14:29:01
+ * @LastEditTime: 2022-10-21 14:29:06
+ */
+export default {
+    title: 'VitePress',
+    description: 'Just playing around.'
+}
